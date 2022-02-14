0c9cd05a21db09eec7467d021555f792298d4a4b
Sage200235 <mueezolatunde@gmail.com>
2022-02-14 19:16:32 +0100

    new

---
+++ App.js
@@ -0,0 +1,29 @@
+import Hero from './Components/Hero'
+import Second from './Components/second'
+import Third from './Components/third'
+import Fourth from './Components/fourth'
+import Fifth from './Components/fifth'
+import Sixth from './Components/sixth'
+import Seventh from './Components/seventh'
+import Map from './Components/map'
+import Footer from './Components/footer'
+
+
+
+function App(){
+  return (
+    <>
+      <Hero/>
+      <Second/>
+      <Third/>
+      <Fourth/>
+      <Fifth/>
+      <Sixth/>
+      <Seventh/>
+      <Map/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
